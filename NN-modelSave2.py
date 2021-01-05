8ef238ed913ee9b2744787a0bb2a67318df3fd3d
bailejor <bailejor11@gmail.com>
2021-01-05 10:20:34 -0600

    Add files via upload
    Added files for two neural nets. Modelsave is with all FCT treatments (232). Modelsave2 is with original dataset (around 100 samples).
    RFINV.py contains best performing LR and RF models

---
+++ NN-modelSave2.py
@@ -0,0 +1,109 @@
+import numpy as np
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+from math import sqrt
+from numpy.random import seed
+from keras.regularizers import l1
+import random
+from keras.models import model_from_json
+import os
+import tensorflow as tf
+from sklearn.metrics import accuracy_score, f1_score, make_scorer, roc_auc_score, balanced_accuracy_score, confusion_matrix, precision_score, recall_score, fbeta_score
+from sklearn.model_selection import LeaveOneOut, cross_val_score, cross_val_predict
+from keras.wrappers.scikit_learn import KerasClassifier
+from numpy import mean, std
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import RobustScaler, StandardScaler
+from imblearn.pipeline import Pipeline, make_pipeline
+import random
+
+seed = 6
+np.random.seed(seed)
+
+dataframe = pandas.read_csv("FCTnoST.csv", header=0)
+dataset = dataframe.values
+# split into input (X) and output (Y) variables
+X_orig = dataset[:,1:15].astype(float)
+y_orig = dataset[:,15:16].astype(float)
+
+print(X_orig)
+print(y_orig)
+
+
+def generate_data(data, number_samples): 
+    generated_set = np.empty((0, 15))
+    print(data)
+    ran_sample = data[np.random.randint(data.shape[0], size=1), :]
+    print(ran_sample)
+
+    for i in range(number_samples):
+        ran_sample = data[np.random.randint(data.shape[0], size=1), :]
+        new_row = np.copy(ran_sample)
+        #Depending upon the function change the rate of behavior in test and control
+        #6 is attention, 7 is escape, 8 is tangible
+
+        if ran_sample[0][3] == 1:
+            new_row[0][9] = (np.random.uniform(low=0.83, high=1.17, size=(1,))) * ran_sample[0][9] # 83 to 100 percent random number between IOA error for aggression
+            new_row[0][10] = (np.random.uniform(low=0.83, high=1.17, size=(1,))) * ran_sample[0][10]#random number between IOA and error for aggression
+            generated_set = np.vstack((generated_set, new_row))
+        elif ran_sample[0][4]==1:
+            new_row[0][9] = (np.random.uniform(low=0.83, high=1.17, size=(1,))) * ran_sample[0][9]# 83 to 100 random number bettwen IOA and error for disruption
+            new_row[0][10] = (np.random.uniform(low=0.83, high=1.17, size=(1,))) * ran_sample[0][10]#random number between IOA and error for diruption
+            generated_set = np.vstack((generated_set, new_row))
+        elif ran_sample[0][2] == 1:
+            new_row[0][9] = (np.random.uniform(low=0.85, high=1.15, size=(1,))) * ran_sample[0][9] # 85 to 100 percent random number between IOA error for SIB
+            new_row[0][10] = (np.random.uniform(low=0.85, high=1.15, size=(1,))) * ran_sample[0][10] #random number between IOA and error for SIB
+            generated_set = np.vstack((generated_set, new_row))
+        elif ran_sample[0][5] == 1:
+            new_row[0][9] = (np.random.uniform(low=0.88, high=1.13, size=(1,))) * ran_sample[0][9]#88 to 99.8 random number bettwen IOA and error for other
+            new_row[0][10] = (np.random.uniform(low=0.88, high=1.13, size=(1,))) * ran_sample[0][10]#random number bettwen IOA and error for other
+            generated_set = np.vstack((generated_set, new_row))
+    return generated_set
+
+
+
+################################################################################################################
+def NeuralNet():
+    model = Sequential()
+    model.add(Dense(16, activation='linear', input_dim=X_orig.shape[1]))
+    model.add(Activation('relu'))
+    model.add(Dense(y_orig.shape[1], activation='sigmoid'))
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=[tf.keras.metrics.AUC()])
+    return model
+
+
+
+cv = LeaveOneOut()
+y_true, y_pred = list(), list()
+for train_ix, test_ix in cv.split(X_orig):
+    # split data
+    X_train, X_test = X_orig[train_ix, :], X_orig[test_ix, :]
+    y_train, y_test = y_orig[train_ix], y_orig[test_ix]
+   
+   #Generate New Samples 
+    num_samp = 1000
+    data_send = np.append(X_train, y_train, axis = 1)
+    gen_data = generate_data(data_send, num_samp)
+    retrieved_data = gen_data
+    y_additional = retrieved_data[:, -1]
+    X_additional = retrieved_data[:, :-1]
+
+    X_train = np.vstack((X_train, X_additional))
+    y_additional =y_additional.reshape((-1, 1))
+    print(y_additional)
+    print(y_train)
+    y_train = np.vstack((y_train, y_additional))
+    #Fit model
+    model = KerasClassifier(NeuralNet, epochs=40, batch_size = 15)
+    model.fit(X_train, y_train)
+    # evaluate model
+    yhat = model.predict(X_test)
+    # store
+    y_true.append(y_test[0])
+    y_pred.append(yhat[0])
+print(accuracy_score(y_true, y_pred))
+print(balanced_accuracy_score(y_true, y_pred))
+
--- RFInv.py
+++ RFInv.py
@@ -9,7 +9,8 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.dummy import DummyClassifier
-from sklearn.metrics import accuracy_score, f1_score, make_scorer, roc_auc_score, balanced_accuracy_score, confusion_matrix, precision_score, recall_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score, make_scorer, roc_auc_score, balanced_accuracy_score, confusion_matrix, precision_score, recall_score, fbeta_score
 from sklearn.model_selection import cross_val_score, GridSearchCV, KFold, cross_validate, StratifiedKFold
 from sklearn.model_selection import cross_val_predict, train_test_split
 from sklearn.impute import SimpleImputer
@@ -19,19 +20,26 @@ import pandas as pd
 import numpy as np
 from numpy import mean
 from imblearn.over_sampling import SMOTE, ADASYN, BorderlineSMOTE, KMeansSMOTE, SVMSMOTE
+from imblearn.combine import SMOTEENN, SMOTETomek
 from imblearn.metrics import geometric_mean_score
 from imblearn.pipeline import Pipeline, make_pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_selection import RFE
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+from keras.wrappers.scikit_learn import KerasClassifier
 
 
-
-dataframe = pd.read_csv("FCTnoST.csv", header = 0)
+dataframe = pd.read_csv("FCTnoST2.csv", header = 0)
 dataset = dataframe.values
 
-X_train = dataframe.iloc[:,1:19]
-y_train = dataset[:,19:20].astype(float)
+X_train = dataframe.iloc[:,1:18]
+y_train = dataset[:,18:19].astype(float)
 
+y_vals = dataframe.iloc[:, 18:19]
+print(y_vals.value_counts())
 
 pca = PCA(n_components = 3)
 #X_train =pca.fit_transform(X_train)
@@ -41,26 +49,38 @@ seed = 6
 
 
 #GET CATERGORICAL FEATURES SEPARATED FROM CONTINUOUS, scale continuous, smotenc with all
-smote_value = 0.9
+smote_value = 0.55
 print("smote value is " + str(smote_value))
 
-sm = ADASYN(random_state = seed)
-rfe = RFE(estimator=DecisionTreeClassifier(), n_features_to_select= 16)
-#Categorical feature pipeline
+sm = KMeansSMOTE(random_state = seed, sampling_strategy = smote_value, cluster_balance_threshold = 0.3)
+rfe = RFE(estimator=DecisionTreeClassifier(), n_features_to_select= 17)
+inpt = 17
+def create_model(x):
+  def bm():
+    clf = Sequential()
+    clf.add(Dense(9, activation='relu', input_dim = x))
+    clf.add(Dense(9, activation='relu'))
+    clf.add(Dense(2, activation = 'sigmoid'))
+    clf.compile(loss = 'categorical_crossentropy', optimizer = SGD())
+    return model
+  return bm
 
 #Scale continuous features only
-preprocessor = ColumnTransformer(transformers = [('scale_cont', StandardScaler(), [0, 1, 16, 17])], remainder = 'passthrough')
-
+preprocessor = ColumnTransformer(transformers = [('scale_cont', StandardScaler(), [0, 1, 14, 15])], remainder = 'passthrough')
+print(preprocessor)
+clf = KerasClassifier(build_fn = create_model(inpt), verbose = 0)
+clf_params = {'clf__epochs': [50, 100]}
+pipeline20 = Pipeline([('preprocessor', preprocessor), ('rfe', rfe), ('clf', clf)])
 
 kNN = KNeighborsClassifier()
 pipeline1 = Pipeline([
-	('preprocessor', preprocessor), ('rfe', rfe), ('sm', sm), ('pca', pca), ('kNN', kNN)])
+	('preprocessor', preprocessor), ('kNN', kNN)])
 kNN_par= {'kNN__n_neighbors':[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 20, 22, 28], 'kNN__weights': ['uniform', 'distance'],
 'kNN__algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'], 'kNN__leaf_size': [10, 20, 30, 40, 50],
 'kNN__metric': ['minkowski']}
 
 rfc =RandomForestClassifier()
-pipeline2 = Pipeline([('rfc', rfc)])
+pipeline2 = Pipeline([('preprocessor', preprocessor), ('rfc', rfc)])
 param_grid = {'rfc__bootstrap': [True],
               'rfc__n_jobs':[-1],
               'rfc__criterion' :['gini'],
@@ -69,10 +89,18 @@ param_grid = {'rfc__bootstrap': [True],
               'rfc__min_samples_leaf': [1, 2, 4],
               'rfc__min_samples_split': [2, 5, 10],
               'rfc__n_estimators': [100, 400],
-              'rfc__class_weight': [{1:1, 0: 1.5}, {1:1, 0: 2}, {1:1, 0: 3}, {1:1, 0: 4}]}
+              'rfc__class_weight':[{0:2.46, 1:1}]}
+
+lr = LogisticRegression()
+pipeline15 = Pipeline([('preprocessor', preprocessor), ('sm', sm), ('lr', lr)])
+lr_params = {'lr__penalty':['none'], 'lr__max_iter': [2000]}
+
+
+pipeline16 = Pipeline([('preprocessor', preprocessor), ('lr', lr)])
+lr_params2 = {'lr__penalty':['elasticnet'], 'lr__solver':['saga'], 'lr__l1_ratio':[0.5], 'lr__max_iter': [2000], 'lr__class_weight':[{0:1, 1:1}, {0:2, 1:1}, {0:3, 1:1}]}
 
 mlp = MLPClassifier(random_state=seed)
-pipeline8 = Pipeline([('preprocessor', preprocessor), ('sm', sm), ('rfe', rfe), ('mlp', mlp)])
+pipeline8 = Pipeline([('preprocessor', preprocessor), ('mlp', mlp)])
 parameter_space = {'mlp__hidden_layer_sizes': [(10, 10, 10), (5, 5), (10,10), (10,10,10, 10, 10)],
      'mlp__activation': ['relu'],
      'mlp__solver': ['adam', 'sgd'],
@@ -82,7 +110,7 @@ parameter_space = {'mlp__hidden_layer_sizes': [(10, 10, 10), (5, 5), (10,10), (1
      'mlp__early_stopping':[False]}
 
 gbm = GradientBoostingClassifier()
-pipeline3 = Pipeline([('preprocessor', preprocessor), ('sm', sm), ('rfe', rfe), ('gbm', gbm)])
+pipeline3 = Pipeline([ ('preprocessor', preprocessor), ('sm', sm), ('gbm', gbm)])
 param = {"gbm__loss":["deviance"],
     "gbm__learning_rate": [0.1, 0.05, 0.01],
     "gbm__min_samples_split": [2, 3, 4, 5],
@@ -94,7 +122,7 @@ param = {"gbm__loss":["deviance"],
     }
 
 dt = DecisionTreeClassifier()
-pipeline11 = Pipeline([('preprocessor', preprocessor), ('rfe', rfe), ('dt', dt)])
+pipeline11 = Pipeline([('preprocessor', preprocessor), ('dt', dt)])
 dt_params = {'dt__max_depth': [2, 3, 4, 5, 6, 7, 8, 9, 10],
       'dt__criterion':['gini', 'entropy'],
       'dt__max_features':['auto'],
@@ -106,12 +134,12 @@ dt_params = {'dt__max_depth': [2, 3, 4, 5, 6, 7, 8, 9, 10],
 
 
 svm = SVC()
-pipeline4 = Pipeline([('preprocessor', preprocessor), ('rfe', rfe), ('pca', pca), ('svm', svm)])
+pipeline4 = Pipeline([('preprocessor', preprocessor), ('svm', svm)])
 tuned_parameters = {'svm__kernel':('linear', 'rbf'), 'svm__C':(2, 1.5, 1, 0.25, 0.5, 0.75, 0.6, 0.8, 0.9), 'svm__gamma': [0.1, 1, 10, 50, 100, 1000],
-'svm__class_weight': [{0:1, 1: 0.5}, {0:2, 1:1}, {0:3, 1:1}]}
+'svm__class_weight': [{1:1, 0: 0.6}]}
 
 NB = GaussianNB(priors = [0.37, 0.63])
-pipeline5 = Pipeline([('preprocessor', preprocessor), ('sm', sm), ('rfe', rfe), ('NB', NB)])
+pipeline5 = Pipeline([('preprocessor', preprocessor), ('NB', NB)])
 nb_params = {'NB__var_smoothing': [0.9, 0.5, 0.3, 0.1, 0.01, 0.001]}
 
 
@@ -128,23 +156,32 @@ pipeline9 = Pipeline([('sm', sm), ('dc', dc)])
 dummy_params3 = {'dc__strategy': ['constant'], 'dc__constant':[0]}
 
 ada = AdaBoostClassifier()
-pipeline7 = Pipeline([('preprocessor', preprocessor), ('sm', sm), ('rfe', rfe), ('ada', ada)])
+pipeline7 = Pipeline([('preprocessor', preprocessor), ('ada', ada)])
 ada_params = {'ada__n_estimators': [50, 100, 200], 'ada__learning_rate':[1, 0.01, 0.001, 0.5]}
 
 
 inner_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
 outer_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
 
-f1_scorer = make_scorer(f1_score)
+
+def tn(y_true, y_pred): return confusion_matrix(y_true, y_pred)[0, 0]
+def tp(y_true, y_pred): return confusion_matrix(y_true, y_pred)[1, 1]
+def fp(y_true, y_pred): return confusion_matrix(y_true, y_pred)[1, 0]
+def fn(y_true, y_pred): return confusion_matrix(y_true, y_pred)[0, 1]
+
+f1_scorer = make_scorer(f1_score, pos_label = 0)
 roc_auc = make_scorer(roc_auc_score)
 balanced_accuracy = make_scorer(balanced_accuracy_score) 
 geo = make_scorer(geometric_mean_score) 
 accuracy = make_scorer(accuracy_score)
+fbeta = make_scorer(fbeta_score, beta = 2, pos_label=0)
+
+
 
 
 models = []
 #models.append(('gbm', GridSearchCV(pipeline3, param, cv=inner_cv, n_jobs=-1, scoring = geo)))
-models.append(('rfc', GridSearchCV(pipeline2, param_grid, cv=inner_cv, n_jobs=-1, scoring = geo)))
+#models.append(('rfc', GridSearchCV(pipeline2, param_grid, cv=inner_cv, n_jobs=-1, scoring = fbeta)))
 #models.append(('kNN', GridSearchCV(pipeline1, kNN_par, cv=inner_cv, n_jobs=-1, scoring = accuracy)))
 #models.append(('dt', GridSearchCV(pipeline11, dt_params, cv=inner_cv, n_jobs = -1, scoring = accuracy)))
 #models.append(('svm', GridSearchCV(pipeline4, tuned_parameters, cv=inner_cv, n_jobs=-1, scoring = accuracy)))
@@ -153,12 +190,12 @@ models.append(('rfc', GridSearchCV(pipeline2, param_grid, cv=inner_cv, n_jobs=-1
 #models.append(('Always Predicts FCT Success', GridSearchCV(pipeline6, dummy_params, cv=inner_cv, n_jobs=-1)))
 #models.append(('Balanced FCT Prediction', GridSearchCV(pipeline10, dummy_params2, cv=inner_cv, n_jobs=-1)))
 #models.append(('Always Predicts FCT Failure', GridSearchCV(pipeline9, dummy_params3, cv=inner_cv, n_jobs=-1)))
-#models.append(('ada', GridSearchCV(pipeline7, ada_params, cv=inner_cv, n_jobs=-1, scoring = geo)))
+#models.append(('ada', GridSearchCV(pipeline7, ada_params, cv=inner_cv, n_jobs=-1, scoring = accuracy)))
+#models.append(('lr', GridSearchCV(pipeline15, lr_params, cv=inner_cv, n_jobs=-1, scoring = fbeta)))
+#models.append(('lr', GridSearchCV(pipeline16, lr_params2, cv=inner_cv, n_jobs=-1, scoring = jaccard)))
+models.append(('clf', GridSearchCV(pipeline20, clf_params, cv=inner_cv, n_jobs=-1, scoring = accuracy)))
 
-def tn(y_true, y_pred): return confusion_matrix(y_true, y_pred)[0, 0]
-def tp(y_true, y_pred): return confusion_matrix(y_true, y_pred)[1, 1]
-def fp(y_true, y_pred): return confusion_matrix(y_true, y_pred)[1, 0]
-def fn(y_true, y_pred): return confusion_matrix(y_true, y_pred)[0, 1]
+
 
 scoring = {'accuracy': make_scorer(accuracy_score), 
 'f1': make_scorer(f1_score, pos_label = 0), 
@@ -170,6 +207,7 @@ scoring = {'accuracy': make_scorer(accuracy_score),
 'FCT_fail_Recall' : make_scorer(recall_score, pos_label=0),
 'FCT_Suc_Precision': make_scorer(precision_score, pos_label=1),
 'FCT_Suc_Recall' : make_scorer(recall_score, pos_label=1),
+'AUC_ROC': make_scorer(roc_auc_score)
 }
 
 
@@ -189,3 +227,27 @@ for name, model in models:
   for metric_name in results.keys():
     average_score = np.average(results[metric_name])
     print('%s : %f' % (metric_name, average_score))
+
+
+tang_tp = 58
+tang_fn = 13
+tang_fp = 16
+tang_tn = 16
+
+tang_precis = (tang_tp)/(tang_tp + tang_fp)
+tang_recall = (tang_tp)/(tang_tp + tang_fn)
+tang_f1 = (2* tang_precis * tang_recall) / (tang_precis + tang_recall)
+
+neg_recall = (tang_tn)/(tang_tn + tang_fp)
+#Neg recall is same thing as specificity or true negative rate
+#Recall is same thing as true positive rate
+
+balanced_falligant = (tang_recall + neg_recall) / 2
+print("Precision for Falligant et al. was " + str(tang_precis))
+print("Recall for Falligant et al. was " + str(tang_recall))
+print("The F1 score for Falligan et al. was " + str(tang_f1))
+print("The balanced accuracy for Falligant et al. was " + str(balanced_falligant))
+print('NPV average for Falligant et al. was 0.56, average PPV was 0.79')
+
+
+
